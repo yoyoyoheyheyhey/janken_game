5e874d1881b6a5973ac73cbf1af620fa9bdabc94
Yohei Goto <yohei_goto@diveintocode.jp>
2020-01-10 20:41:15 +0900

    modify: Jankenはモジュールにしてみる

--- janken.rb
+++ janken.rb
@@ -1,73 +1,88 @@
+
+module Janken
+  HANDS = { "グー": 0, "チョキ": 1, "パー": 2 }.freeze
+
+  def hands
+    HANDS.values
+  end
+
+  def hand_name(hand)
+    HANDS.key(hand)
+  end
+
+  class << self
+    def pon(player_hand, enemy_hand)
+      result = judge(player_hand, enemy_hand)
+      case result
+      when 2
+        puts "あなたの勝ちです"
+        false
+      when 1
+        puts "あなたの負けです"
+        false
+      when 0
+        puts "あいこです"
+        true
+      end
+    end
+
+    def judge(player_hand, enemy_hand)
+      (player_hand - enemy_hand + 3) % 3
+    end
+
+    private :judge
+  end
+end
+
 class Player
+  include Janken
+
   def hand
     while true do
       puts "数字を入力してください。"
-      Janken::HANDS.each do |k, v|
+      HANDS.each do |k, v|
         puts "#{v}:#{k}"
       end
 
       hand = gets.chomp.to_i
 
       if valid?(hand)
-        puts "あなたは#{Janken::HANDS.key(hand)}を出しました"
+        puts "あなたは#{hand_name(hand)}を出しました"
         break
       else
-        puts "【Warning】#{Janken::HANDS.values}の数字を入力してください。"
+        puts "【Warning】#{hands}の数字を入力してください。"
       end
     end
 
     hand
   end
 
-  private 
-
   def valid?(player_hand)
-    Janken::HANDS.values.include?(player_hand) ? true : false
+    hands.include?(player_hand) ? true : false
   end
+
+  private :valid?
 end
 
 class Enemy
+  include Janken
+
   def hand
-    hand = Janken::HANDS.values.sample
-    puts "PCは#{Janken::HANDS.key(hand)}を出しました"
+    hand = hands.sample
+    puts "PCは#{hand_name(hand)}を出しました"
     hand
   end
 end
 
-class Janken
-  HANDS = { "グー": 0, "チョキ": 1, "パー": 2 }.freeze
-
-  def pon(player_hand, enemy_hand)
-    result = judge(player_hand, enemy_hand)
-    case result
-    when 2
-      puts "あなたの勝ちです"
-      false
-    when 1
-      puts "あなたの負けです"
-      false
-    when 0
-      puts "あいこです"
-      true
-    end
-  end
-
-  private
-
-  def judge(player_hand, enemy_hand)
-    (player_hand - enemy_hand + 3) % 3
-  end
-end
 
 def main
   player = Player.new
   enemy = Enemy.new
-  janken = Janken.new
 
   next_game = true
 
   while next_game
-    next_game = janken.pon(player.hand, enemy.hand)
+    next_game = Janken.pon(player.hand, enemy.hand)
   end
 end
 
